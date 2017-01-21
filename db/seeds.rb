e9633a8faec8b10b67e3cfb476c7b9950cff1d35
Yammy <yammyxing@foxmail.com>
2017-01-22 01:11:41 +0800

    revise seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,33 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts 'Hello World!'
-puts '這個種子檔會自動建立一個admin帳號, 並且随机創建 10 個jobs，10个隐藏的jobs'
+puts '這個種子檔會自動建立一個admin帳號, 並且随机創建 20個jobs，15个隐藏的jobs'
 
 create_account = User.create([email: 'admin@gmail.com', password: '111111', password_confirmation: '111111', is_admin: 'true'])
 puts 'Admin account is created successfully!'
 
 job_info = [
-  '招聘RoR工程师',
-  '招聘文案设计',
-  '招聘UI设计师',
-  '招聘Android开发工程师',
-  '招聘产品经理',
-  '招聘前端开发工程师',
-  '招聘市场营销',
-  '招聘php后台研发工程师',
-  '招聘python工程师',
-  '招聘高级JAVA研发工程师',
-  '招聘高级数据挖掘工程师',
-  '招聘高级客服经理'
+  'RoR developer wanted',
+  'IOS developer wanted',
+  'Android developer wanted',
+  'Web developer wanted',
+  'Copy writer wanted',
+  'UI designer wanted',
+  'Customer service manager wanted',
+  'Product manager wanted',
+  'Front-end engineer wanted',
+  'Market analysis manager wanted',
+  'php developer wanted',
+  'python developer wanted',
+  'JAVA developer wanted',
+  'Data analysis manager wanted',
+  'Facial designer wanted'
 ]
-create_jobs = for i in 1..10 do
-                Job.create!([title: job_info[rand(job_info.length)], description: "这是一个公开的工作", wage_upper_bound: rand(40..79) * 1000, wage_lower_bound: rand(20..39) * 1000, is_hidden: 'false'])
+create_jobs = for i in 1..20 do
+                Job.create!([title: job_info[rand(job_info.length)], description: "public job info", wage_upper_bound: rand(40..79) * 1000, wage_lower_bound: rand(20..39) * 1000, is_hidden: 'false'])
               end
 for i in 1..10 do
-  Job.create!([title: job_info[rand(job_info.length)], description: "这是一个隐藏的工作", wage_upper_bound: rand(40..79) * 1000, wage_lower_bound: rand(20..39) * 1000, is_hidden: 'true'])
+  Job.create!([title: job_info[rand(job_info.length)], description: "hidden job info", wage_upper_bound: rand(40..79) * 1000, wage_lower_bound: rand(20..39) * 1000, is_hidden: 'true'])
 end
 
 puts '10 Hidden jobs created.'
